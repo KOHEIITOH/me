068709f29a62a328ead6284ff87f533de6e1cd49
KOHEIITOH <koheiito39@gmail.com>
2023-08-17 23:48:23 +0900

    [fix] Profile

---
+++ index.js
@@ -0,0 +1,9 @@
+function clickEvent() {
+    const lf = "\n";
+    const jp_text = "伊藤 光平（いとう こうへい）" + lf + "1998年3月9日生まれ" + lf + "埼玉県出身";
+    const en_text = "KOHEI ITOH" + lf + "3/9/1998" + lf + "Saitama (JP)";
+
+    const text = jp_text + lf + lf + lf + en_text;
+
+    alert(text);
+}
