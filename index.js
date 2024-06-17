eaddc0a31362df46e0d4077bcaeeb2bdee0911dd
KOHEIITOH <koheiito39@gmail.com>
2024-06-17 21:47:29 +0900

    jsファイルの修正とインデント修正

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-function clickEvent() {
-    const lf = "\n";
-    const jp_text = "伊藤 光平（いとう こうへい）" + lf + "1998年3月9日生まれ" + lf + "埼玉県出身";
-    const en_text = "KOHEI ITOH" + lf + "3/9/1998" + lf + "Saitama (JP)";
+const profile_element = document.querySelector('.js-click-profile');
 
-    const text = jp_text + lf + lf + lf + en_text;
+profile_element.addEventListener("click", function() {
+  const lf = "\n";
+  const jp_text = "伊藤 光平（いとう こうへい）" + lf + "1998年3月9日生まれ" + lf + "埼玉県出身";
+  const en_text = "KOHEI ITOH" + lf + "3/9/1998" + lf + "Saitama (JP)";
+
+  const text = jp_text + lf + lf + lf + en_text;
+
+  alert(text);
+});
+
+
+const secret_entrance = document.querySelector('.js-secret');
+
+secret_entrance.addEventListener("click", function() {
+  if(!confirm("SECRET ENTRANCE")) {
+    return;
+  }
+
+  const request        = new XMLHttpRequest(),
+        today_datetime = new Date().toLocaleDateString("ja-JP", {year: "numeric", month: "2-digit", day: "2-digit"}).replaceAll('/', '-');
+  let text = "工事中・・・";
+
+  request.open('GET', 'https://holidays-jp.github.io/api/v1/date.json', true);
+  request.responseType = 'json';
+  request.send(null);
+
+  request.onload = function () {
+    const data = this.response;
+
+    console.log(data);
+
+    if (data[today_datetime]) {
+      text = "本日は日本の祝日です！！";
+    }
 
     alert(text);
-}
+  };
 
-function confirmEvent() {
-    if(confirm("SECRET ENTRANCE")) {
-        // const request = new XMLHttpRequest();
- 
-        // request.open('GET', '', true);
-        // request.responseType = 'json';
-        // request.send();
+  request.onerror = function() {
+    alert(text);
+  }
 
-        // request.onload = function () {
-        //     const data = this.response;
-        //     console.log(data);
-        // };
+  request.onabort = function() {
+    alert(text);
+  }
 
-        alert("工事中・・・");
-    }
-}
+  request.ontimeout = function() {
+    alert(text);
+  }
+});
